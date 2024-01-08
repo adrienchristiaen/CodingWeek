1cfecc8016c47b61a8d910ab0050780bcdee73e2
Kelian Moy <kelian.moy@telecomnancy.eu>
2024-01-08 14:02:56 +0100

    Et le controller qui va avec

---
+++ src/main/java/eu/telecomnancy/labfx/NavBarController.java
@@ -0,0 +1,54 @@
+package eu.telecomnancy.labfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class NavBarController {
+
+    @FXML
+    private Button balance;
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button messagerie;
+
+    @FXML
+    private Button profil;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private ImageView searchImg;
+
+    @FXML
+    void goBalance(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goMessagerie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goProfil(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchFor(ActionEvent event) {
+
+    }
+
+}
