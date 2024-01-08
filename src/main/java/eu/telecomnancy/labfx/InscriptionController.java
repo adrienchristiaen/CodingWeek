6daeee020875ce6a90e4f19bf889b81e7a07b762
Christiaën Adrien <adrien.christiaen@telecomnancy.eu>
2024-01-08 18:53:30 +0100

    Ajout d'une méthode pour l'inscription

--- src/main/java/eu/telecomnancy/labfx/InscriptionController.java
+++ src/main/java/eu/telecomnancy/labfx/InscriptionController.java
@@ -27,6 +27,22 @@ public class InscriptionController {
 
     @FXML
     void inscriptionButtonHandler(ActionEvent event) {
+        // On utilise les valeurs des champs pour effectuer une vérification
+        String email = emailField.getText();
+        String identifiant = identifiantField.getText();
+        String motDePasse = motDePasseField.getText();
+        String nom = nomField.getText();
+        String prenom = prenomField.getText();
+        String ville = villeField.getText();
+
+        // Vérification basique de connexion
+        if (email.isEmpty() || identifiant.isEmpty() || motDePasse.isEmpty() || nom.isEmpty() || prenom.isEmpty() || ville.isEmpty()) {
+            // Connexion échouée
+            System.out.println("Veuillez remplir tous les champs");
+        } else {
+            // Connexion réussie
+            System.out.println("Inscription réussie");
+        }
 
     }
 
