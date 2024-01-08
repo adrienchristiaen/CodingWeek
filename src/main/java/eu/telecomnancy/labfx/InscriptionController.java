b94d67b8e8ac169e4f308b6c2e5058e21236cbd2
Christiaën Adrien <adrien.christiaen@telecomnancy.eu>
2024-01-08 17:22:26 +0100

    Ajout de l'onglet Inscription (back et front)

---
+++ src/main/java/eu/telecomnancy/labfx/InscriptionController.java
@@ -0,0 +1,33 @@
+package eu.telecomnancy.labfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class InscriptionController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField identifiantField;
+
+    @FXML
+    private PasswordField motDePasseField;
+
+    @FXML
+    private TextField nomField;
+
+    @FXML
+    private TextField prenomField;
+
+    @FXML
+    private TextField villeField;
+
+    @FXML
+    void inscriptionButtonHandler(ActionEvent event) {
+
+    }
+
+}
