e4fc328878c521ced3a5ac11e0c7675a754444ff
Christiaën Adrien <adrien.christiaen@telecomnancy.eu>
2024-01-08 18:48:13 +0100

    Modification de l'onglet connexion en développant les méthodes

--- src/main/java/eu/telecomnancy/labfx/ConnexionController.java
+++ src/main/java/eu/telecomnancy/labfx/ConnexionController.java
@@ -1,140 +1,88 @@
-package eu.telecomnancy.pcd2k17;
+package eu.telecomnancy.labfx;
 
-import eu.telecomnancy.pcd2k17.model.ContextAPI;
-import eu.telecomnancy.pcd2k17.model.DataBaseInterface;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.gitlab4j.api.GitLabApi;
-import org.gitlab4j.api.GitLabApiClient;
-import org.gitlab4j.api.GitLabApiException;
-import org.gitlab4j.api.models.Permissions;
-import org.gitlab4j.api.models.Project;
-import org.gitlab4j.api.models.User;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
-import java.sql.Connection;
-import java.util.List;
 
-public class ConnectionController {
-
-    final static Logger log = LogManager.getLogger(ConnectionController.class);
+public class ConnexionController {
 
     @FXML
-    private Button connectionButton;
+    private TextField IDField;
+
     @FXML
-    private TextField tokenField;
+    private TextField MdpField;
+
     @FXML
-    private TextField urlField;
+    private Button connexionButton;
+
     @FXML
     private Text errorMessage;
 
-    private GitLabApi gitLabApi;
+    @FXML
+    void connexionButtonHandler(ActionEvent event) {
+        // On utilise les valeurs des champs IDField et MdpField pour effectuer une vérification
+        String identifiant = IDField.getText();
+        String motDePasse = MdpField.getText();
 
-    public void initialize(){
-        tokenField.focusedProperty().addListener((ov, oldV, newV)-> {
-            if (newV){ // si on entre une nouvelle valeur dans les champs, on efface le message d'erreur etc
-                tokenField.setStyle("");
-                urlField.setStyle("");
-                errorMessage.setText("");
-            }
-        });
+        // Vérification basique de connexion
+        if ("votreIdentifiantValide".equals(identifiant) && "votreMotDePasseValide".equals(motDePasse)) {
+            // Connexion réussie
+            errorMessage.setText("Connexion réussie");
+            errorMessage.setStyle("-fx-fill: green;"); // Couleur verte pour le succès
+        } else {
+            // Connexion échouée
+            errorMessage.setText("Identifiant ou mot de passe incorrect");
+            errorMessage.setStyle("-fx-fill: red;"); // Couleur rouge pour l'échec
+        }
     }
-
 
     @FXML
-    public void connectionButtonHandler(ActionEvent evt){
-        if(evt.getSource().equals(connectionButton)){
-            errorMessage.setText("Tentative de connexion");
-            errorMessage.setStyle("-fx-fill:green");
-            if (!("".equals(urlField.getText()))&&!("".equals(tokenField.getText()))) {
-                this.gitLabApi = new GitLabApi(urlField.getText(), tokenField.getText());
-                ContextAPI.setGitLabApi(this.gitLabApi);
-                log.debug("Tentative : url : " + urlField.getText() + " ; " + "Token : " + tokenField.getText());
-                try {
-                    gitLabApi.getUserApi().getCurrentUser();
-                    ContextAPI.getDbInterface().retrieveDataFromGitlab(gitLabApi);
-                    FXMLLoader loader = new FXMLLoader();
-                    loader.setLocation(getClass().getResource("projectsList.fxml"));
-                    Parent root = loader.load();
-                    Stage st = new Stage();
-                    st.setScene(new Scene(root, 912, 643));
-                    st.show();
-                    Stage scene = (Stage) connectionButton.getScene().getWindow();
-                    scene.close();
-                }
-                catch (GitLabApiException e)
-                {
-                    tokenField.setStyle("-fx-border-color: red;");
-                    urlField.setStyle("-fx-border-color: red;");
-                    errorMessage.setStyle("-fx-fill:red");
-                    errorMessage.setText("Erreur de connexion, verifier l'url et le token de connexion");
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-            } else {
-                if ("".equals(urlField.getText())){
-                    urlField.setStyle("-fx-border-color: red;");
-                }
-                if ("".equals(tokenField.getText())){
-                    tokenField.setStyle("-fx-border-color: red;");
-                }
-                errorMessage.setText("Merci d'indiquer un URL et un token de connexion");
-            }
-        }
-    }
-
-    public void testConnection(String url, String token){
-        this.gitLabApi = new GitLabApi(url, token);
-        ContextAPI.setGitLabApi(this.gitLabApi);
+    void redirectionInscription(ActionEvent event) {
         try {
-            System.out.println(gitLabApi.getUserApi().getCurrentUser().getName());
-            System.out.println(gitLabApi.getProjectApi());
-        }
-        catch (Exception e)
-        {
+            // Chargez l'onglet d'inscription depuis le fichier FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("inscription.fxml"));
+            AnchorPane page = loader.load();
+
+            // Récupérez le contrôleur associé à l'onglet d'inscription
+            InscriptionController InscriptionController = loader.getController();
+
+            // Créez une nouvelle scène avec l'onglet d'inscription
+            Scene scene = new Scene(page);
+
+            // Obtenez la fenêtre principale à partir de n'importe quel contrôleur
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Définissez la nouvelle scène dans la fenêtre principale
+            primaryStage.setScene(scene);
+
+            // Affichez la nouvelle scène
+            primaryStage.show();
+        } catch (IOException e) {
             e.printStackTrace();
+            // Gérez l'exception (par exemple, en affichant un message d'erreur à l'utilisateur)
+            showErrorDialog("Erreur de redirection", "Une erreur s'est produite lors de la redirection vers l'onglet d'inscription.");
         }
     }
 
-    public GitLabApi getGitLabApi() {
-        return this.gitLabApi;
-    }
 
-    public TextField getTokenField() {
-        return this.tokenField;
-    }
-
-    public TextField getUrlField() {
-        return this.urlField;
-    }
-
-    public Text getErrorMessage() {
-        return this.errorMessage;
-    }
-
-    public void setGitLabApi(GitLabApi gitLabApi) {
-        this.gitLabApi = gitLabApi;
-    }
-
-    public void setTokenField(TextField token) {
-        this.tokenField = token;
-    }
-
-    public void setUrlField(TextField url) {
-        this.urlField = url;
-    }
+private void showErrorDialog(String title, String message) {
+    Alert alert = new Alert(AlertType.ERROR);
+    alert.setTitle(title);
+    alert.setHeaderText(null);
+    alert.setContentText(message);
+    alert.showAndWait();
+}
 
 }
