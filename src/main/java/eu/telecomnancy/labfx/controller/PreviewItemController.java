1b90b8f5dd7ec9da0c4d5354828c9206ab26f1e8
Kelian Moy <kelian.moy@telecomnancy.eu>
2024-01-10 10:06:26 +0100

    fxml de preview item

---
+++ src/main/java/eu/telecomnancy/labfx/controller/PreviewItemController.java
@@ -0,0 +1,27 @@
+package eu.telecomnancy.labfx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class PreviewItemController {
+
+    @FXML
+    private Label cost;
+
+    @FXML
+    private ImageView imgItem;
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label note;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label ville;
+
+}
